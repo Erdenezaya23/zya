2ce32339d80cf9b37cdc60aa7a3e097f99360f9a
Erdenezaya23 <105829220+Erdenezaya23@users.noreply.github.com>
2022-05-19 08:38:50 +0800

    Add files via upload

---
+++ helllo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("                    -----------  \n");
+	printf("                    |          | \n");
+	printf("                    |          | \n");
+	printf("                    |          | \n");
+	printf("                    -----------  \n");
+	printf("                    **       **  \n");
+	printf("                 ***            *** \n");
+	printf("              ****                ****\n");
+	printf("             ****     O      O     ****\n");
+	printf("             ****                  ****\n");
+	printf("             ****        ^          ****\n");
+	printf("             ****                  ****\n");
+	printf("              ****    \\_____/    ****\n");
+	printf("                 ***            *** \n");
+	printf("                    **       **  \n");
+	printf("                        ***\n");     
+	printf("                        ***\n");
+	printf("                    **       **  \n");
+	printf("                ***              *** \n");
+	printf("            ****         O          ****\n");
+	printf("           ****                      ****\n");
+	printf("           ****          O           ****\n");
+	printf("           ****                      ****\n");
+	printf("           ****          O           ****\n");
+	printf("            ****                    ****\n");
+	printf("               ***                *** \n");
+	printf("                  **           **  \n"); 
+	printf("                        ***\n");
+	return 0;
+	
+}
